e7e306062b1683a41aefb4e7178d8a7a5843bad4
Jakub Wojciechowski <jakub@snaprapid.com>
2018-07-03 15:25:31 +0200

    auto ocr added

---
+++ detekcja.py
@@ -0,0 +1,172 @@
+
+# coding: utf-8
+import os
+import Levenshtein
+import keras
+import tensorflow as tf
+import numpy as np
+from keras import backend as K
+from keras.models import load_model
+import keras.callbacks
+import cv2
+import itertools
+import os
+import glob
+from os.path import join
+
+import argparse
+
+#parser = argparse.ArgumentParser()
+
+#parser.add_argument('-m', '--modelpath', help='path for model', type=str, default='best_weights.hdf5')
+#parser.add_argument('-p', '--picturedir', help='savepath for model')
+#parser.add_argument('-g,', '--gpu', help='Which gpu to use', type=str, default="1")
+
+#args = vars(parser.parse_args())
+#os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '3'
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+#os.environ["CUDA_VISIBLE_DEVICES"]=args['gpu']
+
+letters = sorted([' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3',
+           '4', '5', '6', '7', '8', '9', '0'])
+
+
+class predict:
+
+    incorrect = 0
+
+    def __init__(self):
+        self.config = tf.ConfigProto(allow_soft_placement=True)
+
+    def load(self, model = 'best_weights.hdf5'):
+        tf.logging.set_verbosity(tf.logging.FATAL)
+        self.sess = tf.Session(config=self.config)
+        K.set_session(self.sess)
+        self.model = load_model(model, custom_objects={'<lambda>': lambda y_true, y_pred: y_pred})
+
+    def collect_data(self, dirpath):
+        files = os.listdir(dirpath)
+        self.img_pre = np.ones([len(files), 60, 260], dtype=np.uint8)
+        self.X_data = np.ones([len(files), 260, 60, 1])
+        self.filenames = []
+        for index, file in enumerate(files):
+            filename = file.split('.', 1)[0]
+            self.filenames.append(filename)
+            img_path = join(dirpath, file)
+            img = cv2.imread(img_path, 0)
+            img = cv2.resize(img, (260, 60))
+            self.img_pre[index] = img
+            img = (img.astype(np.float32) / 255)
+            img = np.expand_dims(img.T, axis=2)
+            self.X_data[index] = img
+
+    def single_picture(self, picpath):
+        self.X_data = np.ones([1, 260, 60, 1])
+        self.img_pre = cv2.imread(picpath, 0)
+        img = cv2.resize(self.img_pre, (260, 60))
+        img = (img.astype(np.float32) / 255)
+        img = np.expand_dims(img.T, axis=2)
+        self.X_data[0] = img
+
+    def image_matrix(self, img):
+        self.X_data = np.ones([1, 260, 60, 1])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (260, 60))
+        img = (img.astype(np.float32) / 255)
+        img = np.expand_dims(img.T, axis=2)
+        self.X_data[0] = img
+
+    def decode_batch(self, out):
+        ret = []
+        for j in range(out.shape[0]):
+            out_best = list(np.argmax(out[j, 2:], 1))
+            out_best = [k for k, g in itertools.groupby(out_best)]
+            outstr = ''
+            for c in out_best:
+                if c < len(letters):
+                    outstr += letters[c]
+            ret.append(outstr)
+        return ret
+
+    def decode(self):
+
+        net_inp = self.model.get_layer(name='the_input').input
+        net_out = self.model.get_layer(name='softmax').output
+        net_out_value = self.sess.run(net_out,
+                                      feed_dict={net_inp: self.X_data})
+        self.pred_texts = self.decode_batch(net_out_value)
+
+    def calculate_distance(self, index, debug):
+        self.pred_texts[index] = self.pred_texts[index].replace(" ", "")
+        self.filenames[index] = self.filenames[index].replace(" ", "")
+        if (debug):
+            print("----------------------------------")
+            print(self.pred_texts[index] + " :przewidywany wynik")
+            print(self.filenames[index] + " :prawidlowy wynik")
+
+        distance = Levenshtein.distance(self.filenames[index],
+                                        self.pred_texts[index])
+
+        if (distance != 0):
+            self.incorrect += 1
+            if (debug):
+                print("Blad!")
+        if (debug):
+            print("Dystans: " + str(distance))
+            print("Dotychczas zle: " + str(self.incorrect))
+
+    def calculate_accuracy(self, debug):
+
+        for index, prediction in enumerate(self.pred_texts):
+            self.calculate_distance(index, debug)
+
+        self.samples = len(self.filenames)
+        print("Razem probek: " + str(self.samples))
+        print("Blednych probek: " + str(self.incorrect))
+        accuracy = (self.samples-self.incorrect)/self.samples
+        print("f score :" + str(accuracy))
+        print("----------------------------------")
+
+    def display_results(self):
+
+        for index, prediction in enumerate(self.pred_texts):
+            self.calculate_distance(index)
+            cv2.imshow('image', self.img_pre[index])
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+    def single_result(self):
+        try:
+            print(self.pred_texts)
+            cv2.imshow('image', self.img_pre)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        except AttributeError:
+            print('Najpierw przeprowadz detekcje')
+
+    def return_raw(self):
+        try:
+            return self.pred_texts[0]
+        except AttributeError:
+            print('Najpierw przeprowadz detekcje')
+
+    def ocr(self, img):
+        self.image_matrix(img)
+        self.decode()
+        return self.return_raw()
+
+#test = predict()
+
+#test.load()
+
+# dirpath = '/home/yason/ocr/img/val'
+
+#picpath = '/home/yason/tablice_kilka_nowych/DW 7N777.jpg'
+#test.single_picture(picpath)
+# print(test.ocr(picpath))
+#test.collect_data(dirpath)
+#test.decode()
+#test.display_results()
+# test.single_result()
+#test.calculate_accuracy(False)
+# test.display_debug()
--- oznaczanie.py
+++ oznaczanie.py
@@ -4,96 +4,197 @@ import os
 import sys
 import csv
 import glob
+import argparse
+import pytesseract
+from detekcja import predict
 
-def mouse_clb(event,x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if rbuttondown == True:
-            return
-        if(len(pts[numer]) < 4):
-            global pts
-            pts[numer].append([int(x/skala + roi[0][0]), int(y/skala + roi[0][1])])
-    
-    global zoomed
-    if zoomed == True:
-        return
-    global roi
-    global rbuttondown
-    global skala
-    
-    if event == cv2.EVENT_RBUTTONDOWN:
-        rbuttondown = True
-        roi[0] = int(x/skala), int(y/skala)
-        roi[1] = roi[0]
-    if event == cv2.EVENT_RBUTTONUP:
-        rbuttondown = False
-        roi[1] = int(x/skala), int(y/skala)
-        roi = [np.amin(roi, axis = 0), np.amax(roi, axis = 0)]
-        w, h = abs(roi[1] - roi[0])
-        if(h < 10 or w < 10):
-            return
 
-        skala = np.amin([window_size_x/w/skala, window_size_y/h/skala])
-        zoomed = True
-    if event == cv2.EVENT_MOUSEMOVE:
-        roi[1] = int(x/skala), int(y/skala)
+pred = predict()
+pred.load()
+config = ('--oem 1 --psm 7')
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--index", required=False, default=0,
+	help="index klatki od ktorej zaczynamy")
+ap.add_argument("-z", "--zmiana", default=20,
+	help="path to output label binarizer")
+ap.add_argument("-f", "--folder", type=str, default="",
+	help="nazwa folderu ze zdjeciami")
+ap.add_argument("-v", "--video", type=str, default="",
+	help="nazwa pliku video z folderu filmy")
+args = vars(ap.parse_args())
 
-def wczytaj_dane(f_name):
-    plik = open(CSV_FILE, 'r', encoding='utf-8-sig')
-    CSVreader = csv.reader(plik, delimiter='\t', quotechar='|')
-    wiersz = list(CSVreader)
-        
-    global pts
-    global tablica_znaki
-        
-    for col in wiersz:
-        if(f_name == col[0]):                # wykryty istniejacy wpis
-            tablica_znaki = col[1].split(';')   # odczyt znakow tablicy
-            pts = []
-            for y in range(2, len(col)):
-                p = col[y].split(';')
-                pts.append(np.array([x.split(',') for x in p], dtype=np.int32))
-            if(len(pts) == 0):
-                pts.append([])
-            break       
-        else:
-            pts = [[]]
-            tablica_znaki = [f_name]
-    else:
-        pts = [[]]
-        tablica_znaki = [f_name]
+if(not args["folder"] and not args["video"]):
+    print("Brak okreslonego folderu zdjec lub filmu (-f lub -v)")
+    sys.exit(0)
+if(args["folder"]):
+    film_input = False
+if(args["video"]):
+    film_input = True
+
+ZBIOR_FOLDER = args["folder"] + '/'
+video_nazwa = args["video"]
+klawisz_page_zmiana = int(args["zmiana"])
+index = int(args["index"])
+window_size_x = 1600
+window_size_y = 1200
 
 INPUT_DIR = 'input/'
-TABLICE_DIR = 'tablice/'
-CSV_FILE = 'tablice_oznaczone.csv'
-CURRENT_DIR = os.getcwd()
+OUTPUT_DIR = 'output/'
+
+if(film_input):
+	cap = cv2.VideoCapture('input/filmy/' + video_nazwa)
+	if(not cap.isOpened()):
+		print('Nieprawidlowy plik video')
+		sys.exit(0)
+	ZBIOR_FOLDER = video_nazwa[:-4] + '/'
+else:
+	image_path = sorted(glob.glob(INPUT_DIR + ZBIOR_FOLDER + '*'))
+	if len(image_path) == 0:
+	    print("Brak zdjec w folderze " + INPUT_DIR + ZBIOR_FOLDER)
+	    sys.exit(0)
+
+IMAGES_DIR = 'output/images/' + ZBIOR_FOLDER
+MASKA_DIR = 'output/off_maska/' + ZBIOR_FOLDER
+TABLICE_DIR = 'output/tablice/' + ZBIOR_FOLDER
+CSV_FILE = 'output/tablice_oznaczone.csv'
 
 rbuttondown = False
+mbuttondown = False
 zoomed = False
-window_size_x = 800
-window_size_y = 600
 max_liczba_tablic = 5
 max_liczba_znakow = 9
 tablica_size = (260, 60)
-pkt_tablica = np.float32([[0,0],[tablica_size[0],0], 
-                          [tablica_size[0],tablica_size[1]], [0, tablica_size[1]]])
+pkt_tablica = np.float32([[0, 0], [tablica_size[0], 0],
+                          [tablica_size[0], tablica_size[1]], [0, tablica_size[1]]])
 
-roi = [(0,0), (0,0)]
+roi = [(0, 0), (0, 0)]
+off_roi = [(0, 0), (0, 0)]
 pts = [[] for _ in range(max_liczba_tablic)]
 tablica_znaki = ['' for _ in range(max_liczba_tablic)]
-znak = " 0123456789ABCDEFGHIJKLMNOPRSTUWXYZ"   
+tablica_znaki_tmp = tablica_znaki
+znak = " 0123456789ABCDEFGHIJKLMNOPRSTUVWXYZ"
 skala = 1.0
 numer = 0
-color = [(0,0,255), (0,255,0), (255,0,255), (0,255,255), (255,255,255)] 
+color = [(0, 0, 255), (0, 255, 0), (255, 0, 255), (0, 255, 255), (255, 255, 255)]
 
-if not os.path.exists(CURRENT_DIR + '/' + INPUT_DIR):
-    print("Brak zbioru wejściowego")
+def mouse_clb(event, x, y, flags, param):
+    global roi
+    global off_roi
+    global rbuttondown
+    global mbuttondown
+    global pts
+    global zoomed
+    global skala
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if rbuttondown == True:
+            return
+
+        if(len(pts[numer]) < 4):
+            pts[numer].append(
+                [int(x/skala + roi[0][0]), int(y/skala + roi[0][1])])
+    if event == cv2.EVENT_LBUTTONUP:
+        if(len(pts[numer]) == 4):
+            M = cv2.getPerspectiveTransform(np.asarray(pts[numer], dtype=np.float32), pkt_tablica)
+            dst = cv2.warpPerspective(image, M, (tablica_size[0], tablica_size[1]))
+            text = pred.ocr(dst)
+            #text = pytesseract.image_to_string(dst, config=config)[1:]    
+            tablica_znaki[numer] = text
+            
+
+    if event == cv2.EVENT_MBUTTONDOWN:
+        mbuttondown = True
+        off_roi[0] = off_roi[1] = int(x/skala + roi[0][0]), int(y/skala + roi[0][1])
+    if event == cv2.EVENT_MBUTTONUP:
+        mbuttondown = False
+        off_roi[1] = int(x/skala + roi[0][0]), int(y/skala + roi[0][1])
+        off_roi = [np.amin(off_roi, axis = 0), np.amax(off_roi, axis = 0)]
+        w, h = abs(off_roi[1] - off_roi[0])
+        if(h < 5 or w < 5):
+            return
+        cv2.rectangle(off_maska, tuple(off_roi[0]), tuple(off_roi[1]), (0,0,0), cv2.FILLED)
+
+    if zoomed == False:
+        if event == cv2.EVENT_RBUTTONDOWN:
+            rbuttondown = True
+            roi[0] = roi[1] = int(x/skala), int(y/skala)
+        if event == cv2.EVENT_RBUTTONUP:
+            rbuttondown = False
+            roi[1] = int(max(0, x/skala)), int(max(0, y/skala))
+            roi = [np.amin(roi, axis = 0), np.amax(roi, axis = 0)]
+            w, h = abs(roi[1] - roi[0])
+            if(h < 10 or w < 10):
+                return
+            zoomed = True
+            skala = np.amin([window_size_x/w, window_size_y/h])
+
+    if event == cv2.EVENT_MOUSEMOVE:
+        if zoomed == False:
+            roi[1] = int(x/skala), int(y/skala)
+        off_roi[1] = int(x/skala + roi[0][0]), int(y/skala + roi[0][1])
+
+
+def wczytaj_dane(index):
+    global pts
+    global tablica_znaki
+    global numer
+    global zoomed
+    global roi
+    global off_maska
+    global image
+    global skala
+
+    numer = 0
+    zoomed = False
+    roi = [(0,0), (0,0)]
+    if(film_input):
+        cap.set(1, index)
+        _, image = cap.read()
+        file_folder_name = ZBIOR_FOLDER + str(index) + '.jpg'
+    else:
+        image = cv2.imread(image_path[index], 1)
+        file_folder_name = image_path[index][len(INPUT_DIR):]
+    file_name = file_folder_name.split('/')[-1]
+    skala = np.amin([window_size_x/image.shape[1], window_size_y/image.shape[0]])
+
+    plik = open(CSV_FILE, 'r', encoding='utf-8-sig')
+    CSVreader = csv.reader(plik, delimiter='\t', quotechar='|')
+    wiersz = list(CSVreader)
+
+    pts = [[] for _ in range(max_liczba_tablic)]
+    tablica_znaki = ['' for _ in range(max_liczba_tablic)]
+    for col in wiersz:
+        if(file_folder_name == col[0]):                   	# wykryty istniejacy wpis - zdjecia lub film
+            znaki = col[1].split(';')        				# odczyt znakow tablicy
+            tablica_znaki = ['' for _ in range(max_liczba_tablic)]
+            tablica_znaki[:len(znaki)] = znaki[:max_liczba_tablic]
+            off_maska = cv2.imread(MASKA_DIR + col[0].split('/')[1][:-3] + 'png', 0)
+            if(off_maska is None):
+                print("Nieprawidlowa sciezka dla maski: ", file_folder_name)
+            for y in range(2, len(col)):
+                p = col[y].split(';')
+                pts[y-2] = [list(map(int, x.split(','))) for x in p if not x == '']
+            break
+    else:
+        off_maska = np.ones(image.shape[:2], dtype='uint8')
+
+if not os.path.exists(INPUT_DIR):
+    print("Brak folderu wejsciowego ", INPUT_DIR)
     sys.exit(0)
-if not os.path.exists(CURRENT_DIR + '/' + TABLICE_DIR):
-    print("Brak folderu dla tablic - tworzenie folderu ", TABLICE_DIR[:-1])
-    os.makedirs(CURRENT_DIR + '/' + TABLICE_DIR)
+if not os.path.exists(OUTPUT_DIR):
+    print("Tworzenie folderu ", OUTPUT_DIR)
+    os.makedirs(OUTPUT_DIR)
+if not os.path.exists(IMAGES_DIR):
+    print("Tworzenie folderu ", IMAGES_DIR )
+    os.makedirs(IMAGES_DIR)
+if not os.path.exists(MASKA_DIR):
+    print("Tworzenie folderu ", MASKA_DIR)
+    os.makedirs(MASKA_DIR)
+if not os.path.exists(TABLICE_DIR):
+    print("Tworzenie folderu ", TABLICE_DIR)
+    os.makedirs(TABLICE_DIR)
 
 plik = object()
-index = 0
 try:
     plik = open(CSV_FILE, 'a+', encoding='utf-8-sig')
 except FileNotFoundError:
@@ -101,132 +202,161 @@ except FileNotFoundError:
 else:
     print('Wczytano plik ', CSV_FILE)
     plik.seek(0)
-    index = sum(1 for row in plik)
-print("Liczba oznaczonych zdjec: ", index)
+    print("Liczba oznaczonych zdjec: ", sum(1 for row in plik))
 plik.close()
 
-
-image_path = glob.glob(INPUT_DIR + '*')
-if index >= len(image_path):
-   index = len(image_path)-1 
-file_name = image_path[index][len(INPUT_DIR):]
-image = cv2.imread(image_path[index], 1)
-wczytaj_dane(file_name)
+wczytaj_dane(index)
 
 cv2.namedWindow('image', cv2.WINDOW_AUTOSIZE)
 cv2.setMouseCallback('image', mouse_clb)
 
+
 while (1):
     image_display = image.copy()
-    if(zoomed == False):
-        rows, cols = image_display.shape[:2]
-        skala = np.amin([window_size_x/cols, window_size_y/rows])
-    
+
     if(rbuttondown == True):
         cv2.rectangle(image_display, tuple(roi[0]), tuple(roi[1]), (255,0,0), 2)
+    if(mbuttondown == True):
+        cv2.rectangle(image_display, tuple(off_roi[0]), tuple(off_roi[1]), (255,255,255), 2)
+
+    image_display = cv2.bitwise_and(image_display, image_display, mask=off_maska)
 
     for n, p in enumerate(pts):
+        if(n >= 4):
+            n = 4
         for i in range(1, len(p)):
-            cv2.line(image_display, tuple(p[i]),tuple(p[i-1]), color[n], 2)
+            cv2.line(image_display, tuple(p[i]), tuple(p[i-1]), color[n], 2)
         if len(p) == 4:
-            cv2.line(image_display, tuple(p[3]),tuple(p[0]), color[n], 2)
-    
+            cv2.line(image_display, tuple(p[3]), tuple(p[0]), color[n], 2)
+
     if(zoomed == True):
         image_display = image_display[roi[0][1]:roi[1][1], roi[0][0]:roi[1][0]]
-        
-    image_display = cv2.resize(image_display, (0,0), fx = skala, fy = skala)
-    
-    cv2.putText(image_display, 'Numer: ' + str(index+1), (0,25), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
-    cv2.putText(image_display, 'Tablica: ' + tablica_znaki[numer], (0,50), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
-    cv2.putText(image_display, 'Skala: ' + "%.2f" % skala, (0,75), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
-    
+
+    image_display = cv2.resize(image_display, (0, 0), fx=skala, fy=skala)
+
+    cv2.putText(image_display, 'Indeks: ' + str(index), (0,25), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
+    #cv2.putText(image_display, 'Zbior: ' + ZBIOR_FOLDER[:-1], (0,50), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
+    cv2.putText(image_display, 'Tablica: ' + tablica_znaki[numer], (0,75), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
+    #cv2.putText(image_display, 'N: ' + str(liczba_detekcji), (0,100), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
+    #cv2.putText(image_display, 'Skala: ' + "%.2f" % skala, (0,100), cv2. FONT_HERSHEY_PLAIN, 2, color[numer], 2)
+
     cv2.imshow('image', image_display)
-    
-    c = cv2.waitKey(50)
+
+    c = cv2.waitKey(50) % 255
 
     if(c == -1):
         continue
-    if chr(c) in znak.lower():
+
+    if chr(c) in znak.lower():      # zapis wprowadzanych znaków
         if(len(tablica_znaki[numer]) < max_liczba_znakow):
             tablica_znaki[numer] += chr(c).upper()
 
-    if(c >= 176 and c <= 185):   # numpad 0-9
+    if(c >= 176 and c <= 185):      # numpad 0-9
         if(len(tablica_znaki[numer]) < max_liczba_znakow):
             tablica_znaki[numer] += znak[c-175]
+
     if(c == 27):    # ESC
         break
-    if(c == 225 or c == 226):    # SHIFT lewy lub prawy
+    if(c == 225 or c == 226):       # SHIFT lewy lub prawy
         pts[numer] = []
-    
-    if(c == 227):     # CTRL
+
+    if(c == 9):     # TAB
+        tablica_znaki[numer] = tablica_znaki_tmp[numer]
+
+    if(c == 227):    # CTRL lewy
         zoomed = False
         roi = [(0,0), (0,0)]
+        skala = np.amin([window_size_x/image.shape[1], window_size_y/image.shape[0]])
 
     if(c == 8):     # BACKSPACE
         tablica_znaki[numer] = tablica_znaki[numer][:-1]
-    
-    if(c == 13):    # ENTER                      
-        file_name = image_path[index][len(INPUT_DIR):]
-        str_write = file_name + '\t'
         
-        for n in tablica_znaki:
-            if n == '':
-                continue
-            str_write += n + ';'
-        if(str_write[-1:] == ';'):
-            str_write = str_write[:-1]
-            
-        str_write += '\t'
-        
+    if(c == 13 or c == 10):    # ENTER
+        if(film_input):
+            file_folder_name = ZBIOR_FOLDER + str(index) + '.jpg'
+        else:
+            file_folder_name = image_path[index][len(INPUT_DIR):]
+        str_write = file_folder_name + '\t'     									# kolumna 0 - zapis nazwy pliku
+        str_write += ";".join([t for t in tablica_znaki if not t == '']) + '\t'     # kolumna 1 - zapis opisanych znakow tablic
+        zapis = False
+        for i in range(max_liczba_tablic):
+            if(tablica_znaki[i] != '' or len(pts[i]) == 4):
+                zapis = True
+                tablica_znaki_tmp = tablica_znaki
+                break
+
         for i, p in enumerate(pts):
             if(len(p) != 4):
                 continue
-            for k in range(4):            # współrzędne tablicy rejestracyjnej
-                str_write += str(p[k][0]) + ',' + str(p[k][1]) + ';'
-            str_write = str_write[:-1] + '\t'
+            str_write += ';'.join([','.join(map(str, x)) for x in p]) + '\t'        # kolumny 2+ zapis 4 punktów w każdej kolejnej kolumnie
 
+            if(tablica_znaki[i] == ''):
+                continue
             M = cv2.getPerspectiveTransform(np.asarray(p, dtype=np.float32), pkt_tablica)
             dst = cv2.warpPerspective(image, M, (tablica_size[0], tablica_size[1]))
-            cv2.imwrite(CURRENT_DIR + '/' + TABLICE_DIR + tablica_znaki[i] + '.jpg', dst)
-        
-        str_write = str_write[:-1] + "\n"   # przygotowany wiersz do zapisu
-        
+            cv2.imwrite(TABLICE_DIR + tablica_znaki[i] + '.jpg', dst)
+
         with open(CSV_FILE, 'r') as csvfile:
             csvReader = csv.reader(csvfile)
-            clean_rows = [row for row in csvReader if (row[0].split('\t'))[0] != file_name]
+            clean_rows = [row for row in csvReader if row[0].split('\t')[0] != file_folder_name]
 
         with open(CSV_FILE, 'w') as csvfile:
             csv_writer = csv.writer(csvfile)
             csv_writer.writerows(clean_rows)
-            csvfile.write(str_write)     
-         
-        c = 83 # przewiniecie zdjecia w prawo
-    
+            if(zapis == True):
+                file_name = file_folder_name.split('/')[-1]
+                csvfile.write(str_write[:-1] + '\n')                        # zapis bez ostatniego tabulatora
+                cv2.imwrite(IMAGES_DIR + file_name, image)                  # oryginalny obraz
+                cv2.imwrite(MASKA_DIR + file_name[:-3] + 'png', off_maska)  # maska dla loss weights
+
+        if(film_input):
+            c = 85  # przesuniecie filmu
+        else:
+            c = 83 # przewiniecie zdjecia w prawo
+
+    if(c == 0):     # DEL
+        if(film_input):
+            file_folder_name = ZBIOR_FOLDER + str(index) + '.jpg'
+        else:
+            file_folder_name = image_path[index][len(INPUT_DIR):]
+
+        pts = [[] for _ in range(max_liczba_tablic)]
+        tablica_znaki = ['' for _ in range(max_liczba_tablic)]
+        off_maska = np.ones(image.shape[:2], dtype='uint8')
+
+        with open(CSV_FILE, 'r') as csvfile:
+            csvReader = csv.reader(csvfile)
+            clean_rows = [row for row in csvReader if row[0].split('\t')[0] != file_folder_name]
+
+        with open(CSV_FILE, 'w') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerows(clean_rows)
+
+        file_name = file_folder_name.split('/')[-1]
+        if os.path.exists(IMAGES_DIR + file_name):
+            os.remove(IMAGES_DIR + file_name)
+        if os.path.exists(MASKA_DIR + file_name[:-3] + 'png'):
+            os.remove(MASKA_DIR + file_name[:-3] + 'png')
 
     if(c == 82):     # strzalka w gore
         if(numer+1 < max_liczba_tablic):
             numer += 1
-            if(numer >= len(tablica_znaki)):
-                tablica_znaki.append('')
-            if(numer >= len(pts)):
-                pts.append([])
+
     if(c == 84):    # strzalka w dol
         if(numer > 0):
             numer -= 1
-            
-    if(c in [81, 83]):          # jesli strzalka w lewo lub w prawo
-        tmp_index = index
-        if(c == 81 and index > 0):    # strzalka w lewo
-            index -= 1
-        if(c == 83 and index < len(image_path)-1):    # strzalka w prawo
-            index += 1
-        if(tmp_index != index):     # jesli byla zmiana zdjecia
-            numer = 0
-            zoomed = False
-            roi = [(0,0), (0,0)]
-            image = cv2.imread(image_path[index], 1)
-            file_name = image_path[index][len(INPUT_DIR):]
-            
-            wczytaj_dane(file_name)
+
+    if(c == 81 and index > 0):                                          # strzalka w lewo
+        index -= 1
+        wczytaj_dane(index)
+    if(c == 83 and (film_input == True or index < len(image_path)-1)):  # strzalka w prawo
+        index += 1
+        wczytaj_dane(index)
+    if((c==154 or c == 85) and film_input == True):                                 # page-up
+        index += klawisz_page_zmiana
+        wczytaj_dane(index)
+    if((c == 155 or c == 86) and film_input == True and index > klawisz_page_zmiana): # page-down
+        index-= klawisz_page_zmiana
+        wczytaj_dane(index)
 
 cv2.destroyAllWindows()
